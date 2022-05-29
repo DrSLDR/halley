4dfbbd90bcf66d09ef74766be4f54184a7be65fe
Jonas A. Hultén <jonas@sldr.se>
2022-05-29 16:27:19 +0200

    Added basic logger init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
+    #[cfg(debug_assertions)]
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("trace")).init();
+    #[cfg(not(debug_assertions))]
+    env_logger::init();
+
     println!("Hello, world!");
 }
