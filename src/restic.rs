0054567433b10bd2eaf54c48063d25713bdd9837
Jonas A. Hultén <jonas@sldr.se>
2022-05-29 20:03:28 +0200

    Ensuring Restic is even installed

--- src/restic.rs
+++ src/restic.rs
@@ -1,19 +1,30 @@
 use anyhow;
+use log::{debug};
 use std::process::Command;
 
-pub(crate) fn restic_present() -> anyhow::Result<()> {
+pub(crate) fn present() -> anyhow::Result<()> {
+    let output = Command::new("restic")
+        .arg("version")
+        .output()
+        .expect("Restic is not installed");
+    debug!("status {:?}", output);
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     fn init() {
         let _ = env_logger::builder().is_test(true).try_init();
     }
 
     #[test]
-    fn dense() {
+    fn presence() {
         init();
-        assert_eq!(1, 1);
+        assert!(match present() {
+            Ok(_) => true,
+            Err(_) => false,
+        });
     }
 }
