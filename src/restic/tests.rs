8786a5f54ce1fa8d77df91cce09f4eb2c43f7633
Jonas A. Hultén <jonas@sldr.se>
2022-07-19 17:34:36 +0200

    Removed simulacrum
    Turns out, if you want to do someting, you've gotta do it yourself

--- src/restic/tests.rs
+++ src/restic/tests.rs
@@ -3,77 +3,14 @@ use types::*;
 
 use assert_fs::prelude::*;
 use predicates::prelude::*;
-use simulacrum::*;
 use std::process::Output;
 use tracing::{error, trace, Level};
-
-struct WrappedCallMock {
-    e: Expectations,
-}
-
-impl WrappedCallMock {
-    fn new() -> Self {
-        Self {
-            e: Expectations::new(),
-        }
-    }
-
-    fn then(&mut self) -> &mut Self {
-        self.e.then();
-        self
-    }
-
-    fn expect_arg(&mut self) -> Method<String, Self> {
-        self.e.expect::<String, Self>("arg")
-    }
-
-    fn expect_env(&mut self) -> Method<(String, String), Self> {
-        self.e.expect::<(String, String), Self>("env")
-    }
-}
-
-impl WrappedCall for WrappedCallMock {
-    fn invoke(&mut self) -> Result<Output, std::io::Error> {
-        error!("Something tried to invoke the mock!");
-        Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "not allowed to do that",
-        ))
-    }
-    fn arg(&mut self, arg: String) -> &mut Self {
-        trace!("Adding argument {:?}", arg);
-        self.e.was_called::<String, Self>("arg", arg);
-        self
-    }
-    fn env(&mut self, key: String, value: String) -> &mut Self {
-        trace!("Adding envvar {:?} = {:?}", key, value);
-        self.e
-            .was_called::<(String, String), Self>("env", (key, value));
-        self
-    }
-}
 
 fn log_init() {
     let _ = tracing_subscriber::fmt()
         .with_max_level(Level::TRACE)
         .with_test_writer()
         .try_init();
-}
-
-macro_rules! earg {
-    ($mock:tt, $arg:expr) => {
-        $mock.then().expect_arg().called_once().with($arg)
-    };
-}
-
-macro_rules! eenv {
-    ($mock:tt, $key:expr, $val:expr) => {
-        $mock
-            .then()
-            .expect_env()
-            .called_once()
-            .with(params!($key, $val))
-    };
 }
 
 /*
