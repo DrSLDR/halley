f0dc6e9ff6231d12541d0b594fc671fe6bf64cda
Jonas A. Hultén <jonas@sldr.se>
2022-07-20 21:20:30 +0200

    Wrote a barebones s3 client init

--- src/s3/mod.rs
+++ src/s3/mod.rs
@@ -4,9 +4,23 @@
 //! repository exists, as well as ferrying objects in and out of the Glacier storage
 //! class.
 
-use rusoto_core::Region;
-use rusoto_s3::S3Client as Client;
+use crate::trace_call;
+use crate::types::{Region, S3Repo};
 
-fn init() -> Client {
-    unimplemented!();
+use rusoto_core::{credential, Client};
+use rusoto_s3::S3Client;
+use tracing::{trace, trace_span};
+
+/// Creates a [`S3Client`] which can be used to communicate with a given repository.
+///
+/// [`S3Client`]: rusoto_s3::S3Client
+fn init(repo: S3Repo) -> S3Client {
+    trace_call!("init", "called with {:?}", repo);
+    S3Client::new_with_client(
+        Client::new_with(
+            credential::StaticProvider::new_minimal(repo.key.id, repo.key.secret),
+            rusoto_core::HttpClient::new().unwrap(),
+        ),
+        repo.region,
+    )
 }
