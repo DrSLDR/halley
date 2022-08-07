c2d097e7dc7b55aed37e8fc8cf6923240a533e82
Jonas A. Hultén <jonas@sldr.se>
2022-08-07 14:38:12 +0200

    macro abstraction

--- src/s3/tests.rs
+++ src/s3/tests.rs
@@ -19,12 +19,19 @@ fn spawn_handler() {
     let _h: S3Handler = S3Handler::new(repo);
 }
 
+macro_rules! make_s3_client {
+    ($rd:expr, $reg:expr) => {
+        S3Client::new_with($rd, rusoto_mock::MockCredentialsProvider, $reg.clone())
+    };
+    ($reg:expr) => {
+        make_s3_client!(rusoto_mock::MockRequestDispatcher::default(), $reg)
+    };
+}
+
 #[tokio::test]
 async fn bucket_exists() {
-    let cp = rusoto_mock::MockCredentialsProvider;
-    let rd = rusoto_mock::MockRequestDispatcher::default();
     let repo = get_s3_repo!();
-    let client = S3Client::new_with(rd, cp, repo.region.clone());
+    let client = make_s3_client!(repo.region);
     let h = S3Handler::new_with_client(repo, client);
 
     assert!(h.bucket_exists().await);
