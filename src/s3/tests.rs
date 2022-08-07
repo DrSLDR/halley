fd9898e1137bbc400179c77d5c03ebbd8c50cb30
Jonas A. Hultén <jonas@sldr.se>
2022-08-07 14:21:56 +0200

    Macro for repo definition

--- src/s3/tests.rs
+++ src/s3/tests.rs
@@ -4,10 +4,17 @@ use crate::util::test_utils::*;
 
 use rusoto_mock;
 
+macro_rules! get_s3_repo {
+    () => {
+        match s3_repo_def() {
+            Repo::S3 { data } => data,
+            _ => unimplemented!(),
+        }
+    };
+}
+
 #[test]
 fn spawn_handler() {
-    let _h: S3Handler = match s3_repo_def() {
-        Repo::S3 { data } => S3Handler::new(data),
-        _ => unimplemented!(),
-    };
+    let repo = get_s3_repo!();
+    let _h: S3Handler = S3Handler::new(repo);
 }
