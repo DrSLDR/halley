d4ae455e21142b3a93566b6f5e3ff09b82287b5a
Jonas A. Hultén <jonas@sldr.se>
2022-09-18 21:57:19 +0200

    Added a repository configuration

--- src/types.rs
+++ src/types.rs
@@ -89,6 +89,7 @@ pub enum Repo {
 pub struct Config {
     statefile_name: String,
     s3_buckets: Option<Vec<BucketConfig>>,
+    repositories: Option<Vec<RepoConfig>>,
 }
 
 impl Default for Config {
@@ -96,6 +97,7 @@ impl Default for Config {
         Self {
             statefile_name: "halley".to_string(),
             s3_buckets: Some(vec![BucketConfig::default()]),
+            repositories: Some(vec![RepoConfig::default()]),
         }
     }
 }
@@ -118,6 +120,28 @@ impl Default for BucketConfig {
             region: "eu-west-1".to_string(),
             bucket_name: "foo".to_string(),
             credentials: AWSKey::default(),
+        }
+    }
+}
+
+/// Configuration for a repository
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct RepoConfig {
+    id: String,
+    paths: Vec<String>,
+    bucket: String,
+    prefix: Option<String>,
+    password: String,
+}
+
+impl Default for RepoConfig {
+    fn default() -> Self {
+        Self {
+            id: "a_repo".to_string(),
+            paths: vec!["/home".to_string()],
+            bucket: "a_bucket".to_string(),
+            prefix: None,
+            password: "foo".to_string(),
         }
     }
 }
