4835e68d8a77c29f0d873a2e1cc868c3e046a730
Jonas A. Hultén <jonas@sldr.se>
2022-09-18 21:29:54 +0200

    Integration test for the default readback

--- src/config/tests.rs
+++ src/config/tests.rs
@@ -2,4 +2,21 @@ use super::*;
 use crate::types::*;
 use crate::util::test_utils::*;
 
+use assert_fs::prelude::*;
+use std::{fs::File, io::Write};
 use tracing::{error, trace};
+
+#[test]
+#[ignore]
+fn integration_default_readback() {
+    log_init();
+    let c = Config::default();
+    let c_string = toml::to_string_pretty(&c).unwrap();
+    let cf = assert_fs::NamedTempFile::new("test.toml").unwrap();
+    let mut cf_handle = File::create(cf.path()).unwrap();
+    cf_handle.write_all(c_string.as_bytes()).unwrap();
+    cf_handle.flush().unwrap();
+    drop(cf_handle);
+    let c_parsed = make_config(cf.path().to_string_lossy().to_string()).unwrap();
+    assert_eq!(c, c_parsed);
+}
