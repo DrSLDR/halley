a9329c35b050091eaa0182bb21c571b44f8bca1d
Jonas A. Hultén <jonas@sldr.se>
2022-11-10 22:11:37 +0100

    Publicized the CLI args structure components

--- src/cli.rs
+++ src/cli.rs
@@ -7,17 +7,17 @@ use clap::{Parser, Subcommand};
 pub struct Args {
     /// Config file, ~/.halley/config.toml by default
     #[arg(short, long)]
-    config: Option<PathBuf>,
+    pub config: Option<PathBuf>,
 
     /// Quiet (sets log level to ERROR)
     ///
     /// Takes precedence over -v or -d
     #[arg(short, long)]
-    quiet: bool,
+    pub quiet: bool,
 
     /// Verbose (sets log level to INFO)
     #[arg(short, long)]
-    verbose: bool,
+    pub verbose: bool,
 
     /// Debug (sets log level to DEBUG, or TRACE if given twice)
     ///
@@ -25,7 +25,7 @@ pub struct Args {
     ///
     /// Be aware that these log levels WILL leak credentials
     #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
+    pub debug: u8,
 }
 
 pub fn parse() -> Args {
