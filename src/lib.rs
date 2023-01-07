21b85602f1614bf3cfbb844b0d4a53a9efd495d7
Jonas A. Hultén <jonas@sldr.se>
2023-01-07 23:10:28 +0100

    Added a lib call to invoke
    This is where the fun begins

--- src/lib.rs
+++ src/lib.rs
@@ -20,32 +20,8 @@ use figment::{
 use std::path::PathBuf;
 use tracing::{debug, error};
 
-pub async fn test_real() -> anyhow::Result<()> {
-    let h = s3::S3Handler::new(S3Repo {
-        url: "s3.fr-par.scw.cloud".to_owned(),
-        bucket: std::env::var("HALLEY_TEST_BUCKET").unwrap_or("testbucket-2".to_owned()),
-        path: match std::env::var("HALLEY_TEST_PATH") {
-            Ok(path) => Some(path),
-            Err(_) => None,
-        },
-        region: Region::Custom {
-            name: "fr-par".to_owned(),
-            endpoint: "s3.fr-par.scw.cloud".to_owned(),
-        },
-        key: AWSKey {
-            id: std::env::var("HALLEY_TEST_KEY").unwrap_or("[redacted]".to_owned()),
-            secret: std::env::var("HALLEY_TEST_SECRET").unwrap_or("[redacted]".to_owned()),
-        },
-        common: RepoCommon {
-            passwd: "test".to_owned(),
-        },
-    });
-
-    h.list_all_objects().await?;
-    h.archive_all_objects().await?;
-    std::thread::sleep(std::time::Duration::from_secs(15));
-    h.restore_all_objects_blocking().await?;
-
+/// Invoke Halley
+pub fn run(spec: RunSpec) -> anyhow::Result<()> {
     Ok(())
 }
 
