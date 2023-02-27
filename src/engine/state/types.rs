f4ac05466e0a24dc3f00caa6ca9a615c9cc590ef
Jonas A. Hultén <jonas@sldr.se>
2023-02-27 17:54:13 +0100

    Added Default impl for the State types

--- src/engine/state/types.rs
+++ src/engine/state/types.rs
@@ -9,10 +9,29 @@ pub(crate) struct State {
     pub(crate) states: Vec<RepoState>,
 }
 
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            version: 1,
+            states: Vec::new(),
+        }
+    }
+}
+
 /// Representation of an individual repository state
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct RepoState {
     pub(crate) id: String,
     pub(crate) time: u64,
     pub(crate) digest: String,
+}
+
+impl Default for RepoState {
+    fn default() -> Self {
+        Self {
+            id: "a_repo".to_string(),
+            time: 0,
+            digest: "xxx".to_string(),
+        }
+    }
 }
