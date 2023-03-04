1e53ca7535863b602bcdb0648c91c8f8fa8370f4
Jonas A. Hultén <jonas@sldr.se>
2023-03-04 23:28:28 +0100

    Added one more readback test

--- src/engine/state/tests.rs
+++ src/engine/state/tests.rs
@@ -22,12 +22,42 @@ fn usable_state_file_dir_path() {
 fn integration_create_statefile_no_repos() {
     log_init();
     let _tdir = assert_fs::TempDir::new().unwrap();
-    let dir = PathBuf::from(_tdir.as_os_str());
-    let fpath = dir.join("sample.state");
+    let fpath = PathBuf::from(_tdir.as_os_str()).join("sample.state");
     let repos: HashMap<String, Repo> = HashMap::new();
     let r = create_statefile(&fpath, &repos);
     assert!(r.is_ok());
     let state = r.unwrap();
     let read_state: State = toml::from_slice(fs::read(fpath).unwrap().as_slice()).unwrap();
     assert_eq!(state, read_state);
+}
+
+#[test]
+#[ignore]
+fn integration_create_statefile_one_repo_three_ways() {
+    log_init();
+    let _tdir = assert_fs::TempDir::new().unwrap();
+    let fpath = PathBuf::from(_tdir.as_os_str()).join("sample.state");
+    let mut repos: HashMap<String, Repo> = HashMap::new();
+    repos.insert(
+        "beef".to_string(),
+        Repo {
+            paths: vec![fpath.to_string_lossy().to_string()],
+            restic: local_repo_def("beef"),
+        },
+    );
+    let mut state: State = State::default();
+    let repo = RepoState::default();
+    state.states.insert("beef".to_string(), repo);
+
+    let create_state = create_statefile(&fpath, &repos).unwrap();
+    assert_eq!(state, create_state);
+
+    let read_state: State = toml::from_slice(fs::read(&fpath).unwrap().as_slice()).unwrap();
+    assert_eq!(state, read_state);
+    assert_eq!(read_state, create_state);
+
+    write_statefile(&fpath, &state).unwrap();
+    let read_state: State = toml::from_slice(fs::read(&fpath).unwrap().as_slice()).unwrap();
+    assert_eq!(state, read_state);
+    assert_eq!(read_state, create_state);
 }
