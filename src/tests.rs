2c658e08603922952c4f244b6c548fbaf19cd2a1
Jonas A. Hultén <jonas@sldr.se>
2023-03-07 12:10:55 +0100

    Testing a file instead of a dir

--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,17 @@
-use crate::types::{VerifiedPath, VerifiedPathError};
+use crate::{
+    types::{VerifiedPath, VerifiedPathError},
+    util::test_utils::log_init,
+};
 
 use super::*;
+
+use std::fs;
 
 use assert_fs;
 
 #[test]
 fn verified_path_dir() {
+    log_init();
     let td = assert_fs::TempDir::new().unwrap();
     let p = PathBuf::from(td.as_os_str());
     let v = VerifiedPath::from_pathbuf(p.clone());
@@ -13,5 +19,22 @@ fn verified_path_dir() {
     drop(td);
     let v = VerifiedPath::from_pathbuf(p);
     assert!(v.is_err());
+    assert_eq!(v.err().unwrap(), VerifiedPathError::DoesNotExist);
+}
+
+#[test]
+fn verified_path_file() {
+    log_init();
+    let td = assert_fs::TempDir::new().unwrap();
+    let p = PathBuf::from(td.as_os_str());
+    let fp = p.join("afile");
+    let v = VerifiedPath::from_pathbuf(fp.clone());
+    assert_eq!(v.err().unwrap(), VerifiedPathError::DoesNotExist);
+    let _f = fs::File::create(&fp).unwrap();
+    let v = VerifiedPath::from_pathbuf(fp.clone());
+    assert!(v.is_ok());
+    drop(_f);
+    drop(td);
+    let v = VerifiedPath::from_pathbuf(fp);
     assert_eq!(v.err().unwrap(), VerifiedPathError::DoesNotExist);
 }
